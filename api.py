bcd11ee8caabd28cec668775aa0b893a423297a6
AasthaJain-05 <aastha0503jain@gmail.com>
2023-09-21 17:07:44 +0530

    test

--- api.py
+++ api.py
@@ -4,7 +4,15 @@ from flask_restful import Api,Resource,reqparse
 
 app = Flask((__name__))
 api=Api(app)
+student_roll =reqparse.RequestParser()
+student_roll.add_argument("roll",type=int,help="none")
 
+class roll(Resource):
+    def post(self):
+        args=student_roll.parse_args()
+        return {"roll":args["roll"]}
+
+api.add_resource(roll,"/roll")
 
 
 if __name__ == '__main__':
