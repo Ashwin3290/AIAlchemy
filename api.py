2159995106b3f9259eab5ce2cf1187c76014bbb3
Ashwin Choubey <ashwinchoubey03092002@gmail.com>
2023-09-21 17:07:45 +0530

    test

--- api.py
+++ api.py
@@ -5,7 +5,15 @@ from flask_restful import Api,Resource,reqparse
 app = Flask((__name__))
 api=Api(app)
 
+student_name=reqparse.RequestParser()
+student_name.add_argument("name",type=str,help="Send student name")
 
+class name(Resource):
+    def post(self):
+        args=student_name.parse_args()
+        processed=args["name"].split("-")
+        return jsonify(processed)
 
+api.add_resource(name,"/name")
 if __name__ == '__main__':
     app.run(debug=True)
